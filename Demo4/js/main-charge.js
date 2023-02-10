106dc929890f2ccc91d99d371fe5ab048653971d
JordanCulqi <105753502+JordanCulqi@users.noreply.github.com>
2023-02-10 16:14:58 -0500

    Update main-charge.js

--- Demo4/js/main-charge.js
+++ Demo4/js/main-charge.js
@@ -19,7 +19,7 @@ window.addEventListener("message", async function (event) {
         if (parameters3DS) {
             let statusCode = null;
             const email = Culqi.token.email;
-            const responseCharge = await generateChargeImpl({ tokenId, email, parameters3DS });
+            const responseCharge = await generateChargeImpl({ tokenId, deviceId, email, parameters3DS });
             statusCode = responseCharge.statusCode;
 
             if (statusCode === 200) {
